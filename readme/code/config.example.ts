1d6bd014d19fabb5bb40eeeafed5516eb2dcf502
mojavemf <funnyfailiers@gmail.com>
2024-03-02 18:40:31 +0000

    Convert to ascii doc

--- readme/code/config.example.ts
+++ readme/code/config.example.ts
@@ -5,20 +5,20 @@ export default {
     Discord: {
         Presence: {
             status: "dnd",
-            activities: [{ name: "discord.gg/economysimulator", type: ActivityType.Watching }],
+            activities: [{ name: "", type: ActivityType.Watching }],
         },
         Authentication: {
             Token: "BOT_TOKEN",
         },
         Guild: {
-            GuildId: "1190080006194479264",
+            GuildId: "",
             Channels: [],
-            AdminRoles: ["1211304987750039602", "1211415282774712410"],
-            AuthorizedUsers: ["454821956223762453", "825452760362385439"],
+            AdminRoles: [],
+            AuthorizedUsers: [],
         },
     },
     Server: {
-        Website: "economy-simulator.org",
+        Website: "example.com",
         Authentication: {
             Headers: {
                 Cookie: "",
